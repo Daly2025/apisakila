54509a327260de879510d2ab330cc434b576bb48
Daly2025 <lydiafdez84@gmail.com>
2025-02-11 19:46:38 +0100

    api

---
+++ actores.php
@@ -0,0 +1,14 @@
+<?php
+include("conexiondb.php");
+$sql="select actor_id, first_name, last_name from actor";
+$result=$conexion->query($sql);
+$datos=$result->fetchAll(PDO::FETCH_ASSOC);
+$json= json_encode($datos);
+
+header("Content-Type: application/json");
+echo $json;
+
+
+
+
+?>
